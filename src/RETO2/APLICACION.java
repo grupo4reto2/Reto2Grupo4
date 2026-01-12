608eb719e76b351895cb3ff1e8562fa2502909ae
grupo4reto2 <grupo4githubreto2@gmail.com>
2026-01-12 04:01:15 +0100

    Update APLICACION.java
    ...

--- src/RETO2/APLICACION.java
+++ src/RETO2/APLICACION.java
@@ -1,18 +1,43 @@
 package RETO2;
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.time.LocalDateTime;
 
-public class APLICACION
-{
+//TODO ESTO ESTO ES UNA PRUEBA//
+
+public class APLICACION {
 
 	private static void mensajeBienvenida () {
 		String negrita = "\033[1m";
-		System.out.println("______________________________");
-		System.out.println(negrita + "\nBIENVENIDO A ELORRIETA CINESA |");
-		System.out.println("______________________________|");
+		System.out.println("_______________________________ ");
+		System.out.println(negrita + "\nBIENVENIDO A \"NOMBRE DEL CINE\" |");
+		System.out.println("_______________________________|");
+	}
+	
+	private static void relacionPeliculas() {
+		
 	}
 
 	public static void main(String[] args) {
+	Scanner teclado = new Scanner(System.in);
 	mensajeBienvenida();
-		
+	teclado.nextLine();
+
+	Sala sala1 = new Sala("Sala 1", 11);
+	Sesion sesion1 = new Sesion(
+			3303,
+			LocalDateTime.of(2026,2,1,13,30,0),
+			LocalDateTime.of(2026,2,1,15,30,0),
+			sala1,
+			10.50,
+			"Pulp fiction");
+			
+	sala1.getSesiones().add(sesion1);
+	
+	System.out.println("Cartelera:");
+	System.out.println("1 - " + sesion1.getPelicula());
+	
 	}
 
 }
