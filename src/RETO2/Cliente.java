9fdc0279fb1f0ebb1c6812142bdba93141698561
grupo4reto2 <grupo4githubreto2@gmail.com>
2026-01-16 16:39:22 +0100

    Update Cliente.java

--- src/RETO2/Cliente.java
+++ src/RETO2/Cliente.java
@@ -14,81 +14,37 @@ public class Cliente {
 		this.contraseña = contraseña;
 	}
 
-
-	
-	
-	
-
 	public String getDni() {
 		return dni;
 	}
-
-
-
-
-
-
+	
 	public void setDni(String dni) {
 		this.dni = dni;
 	}
-
-
-
-
-
+	
 	public String getNombreApellidos() {
 		return nombreApellidos;
 	}
-
-
-
-
-
-
+	
 	public void setNombreApellidos(String nombreApellidos) {
 		this.nombreApellidos = nombreApellidos;
 	}
-
-
-
-
-
 
 	public String getCorreo() {
 		return correo;
 	}
 
-
-
-
-
-
 	public void setCorreo(String correo) {
 		this.correo = correo;
 	}
-
-
-
-
-
 
 	public String getContraseña() {
 		return contraseña;
 	}
 
-
-
-
-
-
 	public void setContraseña(String contraseña) {
 		this.contraseña = contraseña;
 	}
-
-
-
-
-
 
 	@Override 
 	
